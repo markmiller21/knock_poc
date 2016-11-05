0abfe98d4fd89d4bee7ab7c5efc27cbffcecba2e
Mark Miller <mark.kilpatrick.miller@gmail.com>
2016-11-05 18:11:30 -0400

    add validations for required fields in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  # Validations
+  validates :email, :uniqueness => true, :presence => true
 
+  # Associations
   has_many :knockee_meetings, :class_name => 'Meeting', :foreign_key => 'knockee'
   has_many :knocker_meetings, :class_name => 'Meeting', :foreign_key => 'knocker'
 
