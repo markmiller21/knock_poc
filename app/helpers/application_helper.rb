63159f0171bba86b6adc771a4990fc7da1871641
Mark Miller <mark.kilpatrick.miller@gmail.com>
2016-11-05 21:08:01 -0400

    fix routing in avatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def real_img_or_gu_logo(url_path_str)
-    url_path_str.present? ? url_path_str : User::default_your_pic_path
+    url_path_str.present? ? url_path_str : User::default_avatar_path
   end
 end
