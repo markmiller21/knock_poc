1a4c7ed0c3541ff2c0c9e5edbb9401dbcde8953a
yusong1 <bjslxxx@126.com>
2016-11-11 14:22:35 -0500

    solved some issues about Marks comment

--- app/mailers/meeting_setup_mailer.rb
+++ app/mailers/meeting_setup_mailer.rb
@@ -41,6 +41,8 @@ class MeetingSetupMailer < ApplicationMailer
     (price.to_f/dviden.to_f).round(2)
   end
 
+  #this remove the last 6 chars from DateTime ruby class because sometimes there is a UTC or -0400 or something at the end
+  # e.g. '2016-10-16 19:10:12 -0400' will be just '2016-10-16 19:10:12'
   def call_time(time)
     time.to_s[0..-7]
   end
