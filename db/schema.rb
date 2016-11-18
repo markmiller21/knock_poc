3645761e7290afff9ded61e55d444dd81101b688
yusong1 <bjslxxx@126.com>
2016-11-17 20:05:46 -0500

    commit to switch branch

--- db/schema.rb
+++ db/schema.rb
@@ -46,12 +46,12 @@ ActiveRecord::Schema.define(version: 20161118000554) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                            default: "", null: false
+    t.string   "encrypted_password",               default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",                    default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -74,9 +74,11 @@ ActiveRecord::Schema.define(version: 20161118000554) do
     t.string   "resume_path"
     t.string   "common_essay_path"
     t.string   "college_essay_path"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                                    null: false
+    t.datetime "updated_at",                                    null: false
     t.string   "college"
+    t.text     "bio"
+    t.string   "year",                   limit: 5
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
