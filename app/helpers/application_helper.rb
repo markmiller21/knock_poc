0b50c558d35eed00d4420ff61e59dd8095c793f7
Mark Miller <mark.kilpatrick.miller@gmail.com>
2016-11-18 16:56:35 -0500

    make changes based on comments on github

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,7 +7,8 @@ module ApplicationHelper
     cart.map{|i| i["quantity"]}.map(&:to_i).sum
   end
 
+  # converts rails Time object into a digestable format
   def convert_db_time(date)
-  	return date.strftime("%m/%d/%Y %I:%M %p")
+  	return date.strftime("%m/%d/%Y %I:%M %P")
   end
 end
