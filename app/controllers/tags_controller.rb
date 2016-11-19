0cc798313183aa9ba328a63af83c231c137da922
yusong1 <bjslxxx@126.com>
2016-11-19 16:52:04 -0500

    fixed an issue with the SQL query

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,8 @@ class TagsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def search
     term = params[:term]
-    @users = User.joins(:tags).where("activities LIKE '%#{term}%' OR college LIKE '%#{term}%' OR major LIKE '%#{term}%' OR tags.name LIKE '#{term}%'").page(params[:page]).per(10)
+    @users = User.joins("LEFT JOIN tags_users ON tags_users.user_id = users.id LEFT JOIN tags ON
+ tags.id = tags_users.tag_id").where("activities LIKE '%#{term}%' OR college LIKE '%#{term}%' OR major LIKE '%#{term}%' OR tags.name LIKE '%#{term}%'").page(params[:page]).per(10)
     render "users/index"
   end
 end
