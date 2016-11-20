ba5c0983a705d17e31ad03aaba895f88a14f032a
yusong1 <bjslxxx@126.com>
2016-11-20 14:21:00 -0500

    changed teh default url to deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   #config.action_mailer.default_url_options = { :host => '127.0.0.1:3000' }
-  config.action_mailer.default_url_options = { :host => '127.0.0.1:3000' }
+  config.action_mailer.default_url_options = { :host => '52.24.41.20:3000' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "127.0.0.1", :port => 1025 }
 
