0ba51c9a77afff4f5996605536c5034cec988db1
yusong1 <bjslxxx@126.com>
2016-11-22 20:30:52 -0500

    made set up cc better and enhanced the search

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,8 +3,13 @@ class TagsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def search
     term = params[:term]
-    @users = User.joins("LEFT JOIN tags_users ON tags_users.user_id = users.id LEFT JOIN tags ON
- tags.id = tags_users.tag_id").where("activities LIKE '%#{term}%' OR college LIKE '%#{term}%' OR major LIKE '%#{term}%' OR tags.name LIKE '%#{term}%'").page(params[:page]).per(10)
+    if term.present?
+      where = term.split(" ").map{|key_word| "activities LIKE '%#{key_word}%' OR college LIKE '%#{key_word}%' OR major LIKE '%#{key_word}%' OR tags.name LIKE '%#{key_word}%'" }.join(" OR ")
+      @users = User.joins("LEFT JOIN tags_users ON tags_users.user_id = users.id LEFT JOIN tags ON
+ tags.id = tags_users.tag_id").where(where).uniq.page(params[:page]).per(10)
+    else
+      @users = USer.all.page(params[:page]).per(10)
+    end
     render "users/index"
   end
 end
