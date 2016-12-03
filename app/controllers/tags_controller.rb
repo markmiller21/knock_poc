881c8f6a9fd54e23272725974e7336e1a63a6a4f
yusong1 <bjslxxx@126.com>
2016-12-03 16:17:17 -0500

    a

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -4,32 +4,20 @@ class TagsController < ApplicationController
   def search
     term = params[:term]
     if term.present?
-      where = term.split(" ").map{|key_word| "activities LIKE '%#{key_word}%' OR college LIKE '%#{key_word}%' OR major LIKE '%#{key_word}%' OR tags.name LIKE '%#{key_word}%'" }.join(" OR ")
-      @users = User.joins("LEFT JOIN tags_users ON tags_users.user_id = users.id LEFT JOIN tags ON
- tags.id = tags_users.tag_id").where(where).uniq.page(params[:page]).per(10)
-    else
-      @users = USer.all.page(params[:page]).per(10)
+ #      where = term.split(" ").map{|key_word| "activities LIKE '%#{key_word}%' OR college LIKE '%#{key_word}%' OR major LIKE '%#{key_word}%' OR tags.name LIKE '%#{key_word}%'" }.join(" OR ")
+ #      @users = User.joins("LEFT JOIN tags_users ON tags_users.user_id = users.id LEFT JOIN tags ON
+ # tags.id = tags_users.tag_id").where(where).uniq.page(params[:page]).per(10)
+ #    else
+ #      @users = User.all.page(params[:page]).per(10)
+
+
+      users = []
+      [:activities, :college, :major].each do |field_name|
+        users << User.search(query: {match: {"#{field_name}" => {query: params[:term], fuzziness: 2}}}).records.to_a.map {|u| u.id}
+      end
+      tags = Tag.includes(:tags_users).search(query: {match: {name: {query: params[:term], fuzziness: 2}}}).records.to_a.map {|u| u.tags_users.map(&:user_id)}
+      @users = User.where(id: (users + tags).flatten.uniq).page(params[:page]).per(10)
     end
     render "users/index"
-
-    # potentialUserArray = Array.new
-    # searchArray = params[:term].split(' ');
-    # searchArray.each do |term|
-    # 	# run the term through a sql search based on if there are any
-    # 	# tags that match -- something like
-    # 	# tagMatches = Tags.where("name like '%#{term}")
-    # 	tag_matches = Tag.where("name LIKE (?)", "%#{term}%")
-    # 	tag_matches.each do |tag|
-    # 		tag.users.each do |user|
-    # 			potentialUserArray.push(user)
-    # 		end
-    # 	end
-    # end
-    #
-    # # TODO: only show college students
-    # # TODO: Calculate Mode
-    # @users = Kaminari.paginate_array(potentialUserArray.uniq).page(params[:page]).per(12)
-   	# render "users/index"
-
   end
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ApplicationRecord
+  include Elasticsearch::Model
+  include Elasticsearch::Model::Callbacks
+
 	# Associations
   #Same as in User modal
   #has_and_belongs_to_many :users
